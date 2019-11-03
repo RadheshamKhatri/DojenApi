169e63a097e45d450a6dc46a43930f8e57433312
Radhy <Radhy@IMRAN-PC>
2019-11-04 01:14:49 +0500

    Add project files.

---
+++ DojenApi/Controllers/SubscribersController.cs
@@ -0,0 +1,24 @@
+﻿using DojenApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace DojenApi.Controllers
+{
+    public class SubscribersController : ApiController
+    {
+        private KTControlEntities _db;
+        public SubscribersController()
+        {
+            _db = new KTControlEntities();
+        }
+        public IEnumerable<Kt_Subscriber> GetSubscribers()
+        {
+            return _db.Kt_Subscriber.ToList();
+        }
+    }
+}
